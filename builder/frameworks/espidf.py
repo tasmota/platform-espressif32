950535ea9feca57b238378b5350721c0f5efd41e
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-02-23 14:28:40 +0100

    Adjust flash size for Arduino MMU compatibility
    Override 2MB flash size to 4MB for compatibility.

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -388,6 +388,10 @@ def HandleArduinoIDFsettings(env):
         # Fallback to board.json manifest
         if not flash_size:
             flash_size = board.get("upload", {}).get("flash_size", None)
+
+        if flash_size == "2MB":
+            print(f"Info: Detected 2MB flash size setting, override to 4MB for Arduino MMU page size compatibility")
+            flash_size = "4MB"
         
         if flash_size:
             # Configure both string and boolean flash size formats
